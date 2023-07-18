8151a694f02bc4942e19caedcc6a1ceb696ce5a5
MrDloger <108221813+MrDloger@users.noreply.github.com>
2023-07-18 17:49:56 +0300

    Create AddListProperty.php
    Создание свойств списка (инфоблока) с отображением в публичной части

---
+++ AddListProperty.php
@@ -0,0 +1,19 @@
+//Создание свойств списка (инфоблока) с отображением в публичной части
+
+\Bitrix\Main\Loader::includeModule('lists');
+$iBlockId = 45;
+$feald = new CListFieldList($iBlockId);
+$feald->AddField([
+	'NAME' => 'test3',
+	'TYPE' => 'S',
+	'SORT' => 50,
+	'CODE' => 'TEST3',
+	//'ID' => 'PROPERTY_222',
+	'SETTINGS' => [
+		'SHOW_ADD_FORM' => 'Y',
+		'SHOW_EDIT_FORM' =>'Y',
+		'SHOW_FIELD_PREVIEW' => 'Y',
+    'EDIT_READ_ONLY_FIELD' => 'N',
+    'ADD_READ_ONLY_FIELD' => 'N',
+	],
+]);
